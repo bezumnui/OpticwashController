3441ffad718cae4f7acbc644ab3a78dd5f3411f3
bezikmac <bezumnui.mistikgt@gmail.com>
2025-09-04 13:32:47 +0400

    TryGetDeviceByVendor

--- OpticwashController/Program.cs
+++ OpticwashController/Program.cs
@@ -11,7 +11,15 @@ namespace OpticwashController
             {
                 Console.WriteLine($"PortName: {portName}");
             }
-        
+
+            Console.WriteLine("Trying to find device...");
+            
+            int vendorId =  0x4d8;
+
+            if (Utils.TryGetDeviceByVendor(vendorId, out string result))
+            {
+                Console.WriteLine($"Found: {result}");
+            }
             // MDBCommunicator communicator = new MDBCommunicator(new SerialPort("/dev/tty.usbmodem01"), MDBAddress.First);
             // communicator.Start();
             //
--- OpticwashController/Utils.cs
+++ OpticwashController/Utils.cs
@@ -1,10 +1,86 @@
+using System.IO.Ports;
+
 namespace OpticwashController
 {
     public static class Utils
     {
-        public static string GetDeviceByVendor(int vendor)
+        public static bool TryGetDeviceByVendor(int vendor, out string result)
         {
-            throw new NotImplementedException();
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                if (GetDeviceVendor(port) == vendor)
+                {
+                    result = port;
+
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+        
+        private static string AscendToFirstNonHubUsbDevice(string path)
+        {
+            string current = Path.GetFullPath(path);
+
+            while (true)
+            {
+                string vendorPath = Path.Combine(current, "idVendor");
+                string productPath = Path.Combine(current, "idProduct");
+
+                Console.WriteLine($"Processing path: {current}, vendorPath: {vendorPath}, productPath: {productPath}");
+
+                if (File.Exists(vendorPath) && File.Exists(productPath))
+                {
+                    string bDeviceClassPath = Path.Combine(current, "bDeviceClass");
+                    if (File.Exists(bDeviceClassPath))
+                    {
+                        string cls = File.ReadAllText(bDeviceClassPath).Trim();
+                        
+                        if (!string.Equals(cls, "09", StringComparison.OrdinalIgnoreCase))
+                            return current;
+                    }
+                    else
+                    {
+                        return current;
+                    }
+                }
+
+                string parent = Path.GetFullPath(Path.Combine(current, ".."));
+                if (parent == current || parent == "/" || parent == ".")
+                    throw new InvalidOperationException("USB device with idVendor/idProduct not found.");
+                current = parent;
+            }
+        }
+        
+
+        private static int GetDeviceVendor(string device)
+        {
+            string path = $"/sys/class/tty/{device}/device";
+            
+            // string devicePath = Path.GetFullPath(Path.Combine(path, "../../"));
+            string devicePath = AscendToFirstNonHubUsbDevice(path);
+            string vendorPath = Path.Combine(devicePath, "idVendor");
+
+            if (File.Exists(vendorPath) == false)
+                return 0;
+
+            try
+            {
+                string vendorId = File.ReadAllText(vendorPath).Trim();
+
+                if (int.TryParse(vendorId, out int result))
+                    return result;
+
+                return 0;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading vendor ID: " + e.Message);
+                return 0;
+            }
         }
     }
 }
